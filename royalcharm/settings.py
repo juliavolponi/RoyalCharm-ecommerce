2384bf111628f01d57e30385e25b417f49a3b606
Julia <juliadvolponi@gmail.com>
2025-03-05 21:28:19 +0000

    fixed allowed hosts

--- royalcharm/settings.py
+++ royalcharm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g78$mz0^cs7a*&m2$c)7&d0f7bu)6%=a-gcq$**3&1bo-)6%u)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['royalcharm.herokuapp.com']
+ALLOWED_HOSTS = ['royalcharm-75e0ee204fe8.herokuapp.com']
 
 
 # Application definition
