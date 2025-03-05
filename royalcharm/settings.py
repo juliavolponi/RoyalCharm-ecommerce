dd87c700fa508945b9de72585d7c1a4e91186f7f
Julia <juliadvolponi@gmail.com>
2025-03-05 21:19:13 +0000

    removed django-heroku

--- royalcharm/settings.py
+++ royalcharm/settings.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import django_heroku
+
 import os
 from pathlib import Path
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
