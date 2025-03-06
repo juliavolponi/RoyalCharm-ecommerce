5dd4b83bdc21a51a1dbc02e53145709c3bf43479
Julia <juliadvolponi@gmail.com>
2025-03-06 20:33:44 +0000

    fixed import error for product_list in views

--- royalcharm/settings.py
+++ royalcharm/settings.py
@@ -127,3 +127,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
