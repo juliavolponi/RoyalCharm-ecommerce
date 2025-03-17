ef59c9ce0d0ef580c048f5104fce011509d4e7d5
Julia <juliadvolponi@gmail.com>
2025-03-17 02:13:18 +0000

    Fixed static file duplication issue

--- royalcharm/settings.py
+++ royalcharm/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "store/static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'store/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
