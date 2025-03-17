dbf32e0cc36853153eebb38dbc15f354b837c701
Julia <juliadvolponi@gmail.com>
2025-03-17 03:04:20 +0000

    fixed media issue

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import product_list, product_detail, add_to_cart, view_cart, remove_from_cart, checkout, payment_success, update_cart, category_page, register
 
 urlpatterns = [
@@ -16,3 +18,6 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/register/', register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
