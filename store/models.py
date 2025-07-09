fb1a25a425b6b8301edb63ef3f972a2baa5a7b83
Julia <juliadvolponi@gmail.com>
2025-07-09 22:27:14 +0100

    switch image field to URLField and use cloudinary URLs

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
